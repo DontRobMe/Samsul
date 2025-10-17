61db3ac7a9122c01b18945afc0188cd753fb3518
fabri <fabrice.batista@ynov.com>
2025-10-17 10:07:17 +0200

    login page created

--- Dev/frontend/src/router/index.js
+++ Dev/frontend/src/router/index.js
@@ -2,16 +2,27 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Home from '@/views/Home.vue'
 import Reservation from '@/views/Reservation.vue'
 import Paris from '@/views/Paris.vue'
+import Login from '@/views/Login.vue'
 
 const routes = [
   { path: '/', name: 'Home', component: Home },
   { path: '/reservation', name: 'Reservation', component: Reservation },
-  { path: '/paris', name: 'Paris', component: Paris }
+  { path: '/paris', name: 'Paris', component: Paris },
+  { path: '/login', name: 'Login', component: Login }
 ]
 
 const router = createRouter({
   history: createWebHistory(),
   routes
 })
+
+router.beforeEach((to, from, next) => {
+  const isAuthenticated = localStorage.getItem('auth') === 'true';
+  if (!isAuthenticated && to.name !== 'Login') {
+    next({ name: 'Login' });
+  } else {
+    next();
+  }
+});
 
 export default router
